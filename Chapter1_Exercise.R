573a0a6afe6813d8c2d403d84ad539afecc02331
kishoresurana <kishoresurana@gmail.com>
2017-06-11 08:34:46 -0700

    Chapter 3 lab

---
+++ Chapter1_Exercise.R
@@ -0,0 +1,12 @@
+# 1.
+  # a) The sample size n is extremely large, and the number of predictors p is small. - flexible method is better
+  # b) The number of predictors p is extremely large, and the number of observations n is small. - inflexible method is better
+  # c) The relationship between the predictors and response is highly non-linear. - flexible method is better
+  # d) The variance of the error terms, i.e. σ2 = Var(E), is extremely high. - neither would be better, since there is a high rate of irreducible error
+
+# 2.
+  # a) Regression, inference, n=500, p=3
+  # b) Classification, prediction, n=20, p=13
+  # c) Regression, prediction, n=52, p=3
+
+# 3.
